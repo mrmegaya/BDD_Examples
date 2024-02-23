0506644aa393fc5baa599cd8f25984231d66bfda
Megaya Jesu <73550590+mrmegaya@users.noreply.github.com>
2024-02-23 10:51:19 +0530

    Add files via upload
    BDD_Commit

---
+++ BDD_Examples/src/first_Examples/FirstTest.java
@@ -0,0 +1,10 @@
+package first_Examples;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BDD_Examples/src/first_Examples/FirstTry.java
@@ -0,0 +1,12 @@
+package first_Examples;
+
+import org.testng.annotations.Test;
+
+public class FirstTry {
+	@Test
+	public void login()
+	{
+		System.out.println("HI Megaya");
+	}
+
+}
